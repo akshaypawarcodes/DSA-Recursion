30e02de07641dca237c8b57b6a4b09e6111aafeb
Akshay Kumar <109279257+akshaypawarcodes@users.noreply.github.com>
2022-08-16 10:44:41 +0530

    Update FibonacciSeries.java

--- FibonacciSeries.java
+++ FibonacciSeries.java
@@ -2,12 +2,14 @@
 import java.util.*;
 class FibonacciSeries{
     public static int fibo(int n){
-
+        //base case
         if(n == 1 || n == 2){
             return 1;
         }
+        //recursive call
         return (fibo(n-1) + fibo(n-2));
     }
+    //main function
     public static void main (String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
