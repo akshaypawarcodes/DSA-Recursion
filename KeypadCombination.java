2cf55bd8341d72c571708ca1bef3448b264ec3a6
Akshay Kumar <109279257+akshaypawarcodes@users.noreply.github.com>
2022-08-16 10:46:23 +0530

    Update KeypadCombination.java

--- KeypadCombination.java
+++ KeypadCombination.java
@@ -4,15 +4,17 @@ public class KeypadCombination {
     public static String[] keypad={".","abc","def","ghi","jkl","mno","pqrs","tu","vwx","yz"};
 
     public static void printCombination(String str, int indx, String combination){
+        //base case
         if(indx == str.length()){
             System.out.println(combination);
             return;
         }
-
+        
         char currChar=str.charAt(indx);
 
         String mapping=keypad[currChar-'0'];
-
+        
+        //recursive call
         for(int i=0; i<mapping.length(); i++ )
         printCombination(str, indx+1, combination+mapping.charAt(i));
 
